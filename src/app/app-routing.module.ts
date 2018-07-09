93e3adbb719ce16ebac1a884622bb4e1e470e61b
unknown <juanmiguelherbau@gmail.com>
2018-07-09 22:51:54 +0100

    initial commit authentication module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './common/components/home/home.component';
+import { RegisterComponent } from './modules/authentication/components/register/register.component';
+import { LoginComponent } from './modules/authentication/components/login/login.component';
 
 const routes: Routes = [
-
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/components/header/header.component';
 import { HomeComponent } from './common/components/home/home.component';
@@ -14,7 +16,8 @@ import { HomeComponent } from './common/components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthenticationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
