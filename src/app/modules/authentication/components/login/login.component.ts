d2aad6e72eb3932400656b60c6ec7459cb51bde4
Juan Miguel Hernández Baute <juanmiguelherbau@gmail.com>
2018-07-16 15:17:27 +0100

    fix formgroup controls problem

--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { AuthenticationService } from '../../services/authentication.service';
