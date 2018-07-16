0fac51a0b9c0352dbc4f21d34c49d305a82449f7
Juan Miguel Hernández Baute <juanmiguelherbau@gmail.com>
2018-07-16 12:45:41 +0100

    add student component

---
+++ src/app/modules/students/components/add-student/add-student.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { StudentsService } from '../../services/students.service';
+
+@Component({
+  selector: 'app-add-student',
+  templateUrl: './add-student.component.html',
+  styleUrls: ['./add-student.component.css']
+})
+export class AddStudentComponent implements OnInit {
+
+  studentForm: FormGroup;
+  student: any;
+
+  constructor(private sf: FormBuilder,
+              private studentService: StudentsService) { }
+
+  ngOnInit() {
+    this.studentForm = this.sf.group({
+      firstName: ['', Validators.required ],
+      lastName: ['', Validators.required ],
+      email: ['', Validators.email ],
+      course: ['', Validators.required ]
+    });
+  }
+
+  onSubmit(){
+    this.student = this.saveStudent();
+    this.studentService.postStudent(this.student)
+      .subscribe(newStudent => {
+
+      });
+  }
+
+  saveStudent() {
+    const saveStudent = {
+      firstName: this.studentForm.get('firstName').value,
+      lastName: this.studentForm.get('lastName').value,
+      email: this.studentForm.get('email').value,
+      course: this.studentForm.get('course').value
+    };
+    return saveStudent;
+  }
+}
